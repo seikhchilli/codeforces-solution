7085f92b49c65c0d691cb4a1bef26fa01c8c83e4
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-09-29 17:07:28 +0530

    Special Permutation

---
+++ 1454A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin>>t;
+  int n;
+  while(t--){
+    cin>>n;
+    for(int i = 2; i <= n; i++){
+      cout<<i<<" ";
+    }
+    cout<<1<<endl;
+  }
+  return 0;
+}
