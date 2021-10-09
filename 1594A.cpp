af8418c85187d9026cecfd1b6c5085f15f1fdf33
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-10-09 20:25:07 +0530

    Consecutive Sum Riddle

---
+++ 1594A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl '\n'
+#define debug(n) cout<<(n)<<endl;
+const ll INF = 2e18 + 99;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin>>t;
+
+  ll n;
+  while(t--){
+    cin>>n;
+    cout<<(-1 * (n - 1))<<" "<<n<<endl;
+  }
+}
