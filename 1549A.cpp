f018cf4a1c5305086c8194912ae01fe2fc38a97b
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-10-16 10:40:24 +0530

    Gregor and Cryptography

---
+++ 1549A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl '\n'
+#define debug(n) cout<<(n)<<endl;
+const ll INF = 2e18 + 99;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin>>t;
+  int n;
+  while(t--){
+    cin>>n;
+    n--;
+    cout<<2<<" "<<n<<endl;
+  }
+
+}
