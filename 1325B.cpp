c695a8171642d461cd88c750267252f31b45681d
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-10-24 13:34:11 +0530

    CopyCopyCopyCopyCopy

---
+++ 1325B.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl '\n'
+#define debug(n) cout<<(n)<<endl;
+const ll INF = 2e18 + 99;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    set <int, greater<int> > s1;
+    int x;
+    for(int i = 0; i < n; i++){
+      cin>>x;
+      s1.insert(x);
+    }
+    cout<<s1.size()<<endl;
+  }
+
+}
