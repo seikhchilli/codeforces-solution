fa772b76997cac0244b00c8522e23343d426f671
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-11-05 20:55:32 +0530

    Mahmoud and Ehab and the even-odd game

---
+++ 959A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl '\n'
+#define debug(n) cout<<(n)<<endl;
+const ll INF = 2e18 + 99;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n;
+  cin>>n;
+
+  if(n & 1){
+    cout<<"Ehab"<<endl;
+  }
+  else{
+    cout<<"Mahmoud"<<endl;
+  }
+
+}
