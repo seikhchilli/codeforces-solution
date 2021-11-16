ca9667d7223e6c3840eacb5013f6a3090ab89abc
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-11-16 15:33:13 +0530

    Filling Diamonds

---
+++ 1339A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl '\n'
+#define debug(n) cout<<(n)<<endl;
+const ll INF = 2e18 + 99;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    cout<<n<<endl;
+  }
+
+}
