6a9fdea3ac1a6b15b776633c171f489317de5808
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-11-21 18:59:12 +0530

    Vus the Cossack and a Contest

---
+++ 1186A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl '\n'
+#define debug(n) cout<<(n)<<endl;
+const ll INF = 2e18 + 99;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n, m, k;
+  cin>>n>>m>>k;
+  (n <= min(m, k)) ? cout<<"Yes"<<endl : cout<<"No"<<endl;
+
+}
