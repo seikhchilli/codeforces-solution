3cd42a9ecfa8f0730fdb64ba471993525c18dd1e
seikhchilli <saurabhkrsingh2001@gmail.com>
2022-05-21 11:06:12 +0530

    Z mod X = C

---
+++ 1684B.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl '\n'
+#define debug(n) cout<<(n)<<endl;
+const ll INF = 2e18 + 99;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin>>t;
+  while(t--){
+    int a, b, c;
+    cin>>a>>b>>c;
+    cout<<(a+b+c)<<" "<<(b+c)<<" "<<(c)<<endl;
+  }
+
+}
