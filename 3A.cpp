0a85b081b4c28a452c7e529de037a40c9ded5098
Saurabh Kumar <saurabhkrsingh2001@gmail.com>
2024-05-03 19:06:41 +0530

    Shortest path of the king

---
+++ 3A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl "\n"
+#define debug(n) cout<<(n)<<endl;
+#define pb push_back
+const ll INF = 2e18 + 99;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  string s, t;
+  cin>>s>>t;
+
+}
